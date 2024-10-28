459a77b681bf379b4fa6caf5af00c3af20025224
kuraga-2 <denismanarskij@tutamail.com>
2024-10-28 09:31:23 +0300

    with list

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 input_data = open('input.txt', 'r') 
 data = input_data.read()
+l = list()
 a = int(data)
 c = 1
 for i in range( 1 , a + 1):
     c*=i
+    l.append(c)
 output_data = open ('output.txt','w') 
-output_data.write(str(c))
+output_data.write(str(l))
 input_data.close()
 output_data.close()
